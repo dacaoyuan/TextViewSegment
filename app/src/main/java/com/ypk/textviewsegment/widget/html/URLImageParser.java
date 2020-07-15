99d3a769eadea56c2801c77151ea875b0b6de613
yuan_peikai <2908363622@qq.com>
2020-07-15 14:57:57 +0800

    完善了功能

--- app/src/main/java/com/ypk/textviewsegment/widget/html/URLImageParser.java
+++ app/src/main/java/com/ypk/textviewsegment/widget/html/URLImageParser.java
@@ -14,11 +14,14 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
 
+import androidx.core.content.ContextCompat;
+
 import com.laodao.library.utils.BitmapUtil;
 import com.laodao.library.utils.DensityUtils;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.assist.FailReason;
 import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
+import com.ypk.textviewsegment.R;
 import com.ypk.textviewsegment.utils.BitmapUtils;
 
 /**
@@ -45,40 +48,51 @@ public class URLImageParser implements Html.ImageGetter {
     @Override
     public Drawable getDrawable(String source) {
         System.out.println("URLImageParser.getDrawable source=" + source);
-        final URLDrawable urlDrawable = new URLDrawable();
-        ImageLoader.getInstance().loadImage(source, new SimpleImageLoadingListener() {
-            @Override
-            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
-                float picH = loadedImage.getHeight();
-                float picW = loadedImage.getWidth();
-                System.out.println("URLImageParser.onLoadingComplete picW=" + picW + " picH =" + picH);
-                loadedImage = BitmapUtils.zoomImg(loadedImage, width, (int) ((picH / picW) * width));
-                urlDrawable.bitmap = loadedImage;
-
-                int width = loadedImage.getWidth();
-                int height = loadedImage.getHeight();
-                System.out.println("URLImageParser.onLoadingComplete width=" + width);
-                urlDrawable.setBounds(0, 0, width, height);
-                mTextView.invalidate();
-                mTextView.setText(mTextView.getText());
 
 
-            }
+        if (source.startsWith("http")) {
+            final URLDrawable urlDrawable = new URLDrawable();
+            ImageLoader.getInstance().loadImage(source, new SimpleImageLoadingListener() {
+                @Override
+                public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                    float picH = loadedImage.getHeight();
+                    float picW = loadedImage.getWidth();
+                    System.out.println("URLImageParser.onLoadingComplete picW=" + picW + " picH =" + picH);
+                    loadedImage = BitmapUtils.zoomImg(loadedImage, width, (int) ((picH / picW) * width));
+                    urlDrawable.bitmap = loadedImage;
 
-            @Override
-            public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
-                super.onLoadingFailed(imageUri, view, failReason);
-                System.out.println("URLImageParser.onLoadingFailed" + failReason.getCause());
-            }
+                    int width = loadedImage.getWidth();
+                    int height = loadedImage.getHeight();
+                    System.out.println("URLImageParser.onLoadingComplete width=" + width);
+                    System.out.println("URLImageParser.onLoadingComplete height=" + height);
+                    urlDrawable.setBounds(0, 0, width, height);
+                    mTextView.invalidate();
+                    mTextView.setText(mTextView.getText());
 
-            @Override
-            public void onLoadingCancelled(String imageUri, View view) {
-                super.onLoadingCancelled(imageUri, view);
-                System.out.println("URLImageParser.onLoadingCancelled");
-            }
-        });
 
-        return urlDrawable;
+                }
+
+                @Override
+                public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+                    super.onLoadingFailed(imageUri, view, failReason);
+                    System.out.println("URLImageParser.onLoadingFailed" + failReason.getCause());
+                }
+
+                @Override
+                public void onLoadingCancelled(String imageUri, View view) {
+                    super.onLoadingCancelled(imageUri, view);
+                    System.out.println("URLImageParser.onLoadingCancelled");
+                }
+            });
+            return urlDrawable;
+        } else {
+            // urlDrawable.bitmap = BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_huo_yan);
+            int id = Integer.parseInt(source);
+            //Drawable drawable = ContextCompat.getDrawable(context, R.drawable.ic_solid_red_oval);
+            Drawable drawable = ContextCompat.getDrawable(context, id);
+            drawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
+            return drawable;
+        }
 
     }
 
